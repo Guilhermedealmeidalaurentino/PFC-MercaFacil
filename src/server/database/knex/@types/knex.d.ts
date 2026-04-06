a6301b888b1fcde043b245739c6e265463515a1e
Guilherme de Almeida <guilherme.a.laurentino@gmail.com>
2026-04-06 04:16:48 -0300

    feat: config migrates

---
+++ src/server/database/knex/@types/knex.d.ts
@@ -0,0 +1,8 @@
+declare module'knex/types/tables'{
+  interface Tables{
+    //produtos: IProduto
+    //cliente: ICliente
+    //comerciante: IComerciante
+    //usuario: IUsuario
+  }
+}
---
+++ src/server/database/migrations/0000_create_produto.ts
@@ -0,0 +1,29 @@
+import type { Knex } from "knex";
+import { ETablesNames } from "../ETablesNames";
+
+
+export async function up(knex: Knex): Promise<void> {
+  return knex
+    .schema
+    .createTable(ETablesNames.produto, table =>{
+      table.bigIncrements('id').primary().index();
+      table.string('nome',150).index().notNullable();
+      table.double('preco',4).index().notNullable();
+      table.double('estoque',6).index().notNullable();
+    }).then(()=>{
+        console.log(`# Created table ${ETablesNames.produto}`); 
+    })
+
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable(ETablesNames.produto)
+  .then(()=>{
+        console.log(`# Dropped table ${ETablesNames.produto}`); 
+    })
+
+
+}
+
+
